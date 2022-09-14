e036d4ed20fb91170e5a64a5680c8c7488fbaa3d
Mojtaba Michael <mojimich2015@gmail.com>
2022-09-14 14:10:56 +0300

    Handle post data in order pizza view

--- pizza/views.py
+++ pizza/views.py
@@ -7,5 +7,12 @@ def home(request):
 
 
 def order(request):
-    form = PizzaForm()
-    return render(request, 'pizza/order.html', {"pizzaForm": form})
+    if request.method == "POST":
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = f"Thanks for ordering! Your {filled_form.cleaned_data['size']} {filled_form.cleaned_data['topping1']} and {filled_form.cleaned_data['topping2']} pizza is on it's way."
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {"pizzaForm": new_form, "note": note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {"pizzaForm": form})
